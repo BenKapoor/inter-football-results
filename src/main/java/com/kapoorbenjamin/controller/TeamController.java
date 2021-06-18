ccd8eb0241bb63a442f2699fa7edf5cfe72f0c4e
Benjamin <benjaminkapoor@yahoo.fr>
2021-06-18 19:25:37 +0200

    Add backend Match APi

--- src/main/java/com/kapoorbenjamin/controller/TeamController.java
+++ src/main/java/com/kapoorbenjamin/controller/TeamController.java
@@ -1,11 +1,16 @@
 package com.kapoorbenjamin.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kapoorbenjamin.model.Match;
 import com.kapoorbenjamin.model.Team;
 import com.kapoorbenjamin.repository.MatchRepository;
 import com.kapoorbenjamin.repository.TeamRepository;
@@ -31,6 +36,17 @@ public class TeamController {
 		team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4)); // 4 derniers matchs
 
 		return team;
+	}
+
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+
+		return this.matchRepository.getMatchesByTeamBetweenDate(
+				teamName,
+				startDate,
+				endDate);
 	}
 
 }
--- src/main/java/com/kapoorbenjamin/repository/MatchRepository.java
+++ src/main/java/com/kapoorbenjamin/repository/MatchRepository.java
@@ -1,10 +1,13 @@
 package com.kapoorbenjamin.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.kapoorbenjamin.model.Match;
 
@@ -12,7 +15,18 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
 	List<Match> getByHomeTeamOrAwayTeamOrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 
-	// permet d'indiquer le nombre d'item affichable dans la page, ici page 0, 4 items
+	@Query("select m from Match m where (m.homeTeam = :teamName or m.awayTeam = :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+	List<Match> getMatchesByTeamBetweenDate(
+			@Param("teamName") String teamName, 
+			@Param("dateStart") LocalDate dateStart,
+			@Param("dateEnd") LocalDate dateEnd);
+
+//	List<Match> getByHomeTeamAndDateBetweenOrAwayTeamAndDateBetweenOrderByDateDesc(
+//			String teamName1, LocalDate date1, LocalDate date2,
+//			String teamName2, LocalDate date3, LocalDate date4);
+
+	// permet d'indiquer le nombre d'item affichable dans la page, ici page 0, 4
+	// items
 	default List<Match> findLatestMatchesByTeam(String teamName, int count) {
 		return getByHomeTeamOrAwayTeamOrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
 	}
